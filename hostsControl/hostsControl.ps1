f330281793a06f4b3bdbd6e457e96172b61c446d
Shaun Earsom <searsom@icloud.com>
2021-09-28 10:58:03 -0700

    Update hostsControl.ps1

--- hostsControl/hostsControl.ps1
+++ hostsControl/hostsControl.ps1
@@ -3,20 +3,18 @@
 	Insures hosts file has the correct entry for the target server Octopus is working on.
 
 .DESCRIPTION
-    This script will do the following on the script execution server:
-    1. Open a Powershell instance in admin mode.
-    2. Comments any un-commented lines in the hosts file.
-    3. Uncomments host entry we need for this run.  If it doesn't exist, it is created.
-    4. Exit the Powershell instance.
-
-.PARAMETER octoHost
-	Passed to Get-Content | Set-Content to alter the hosts file.
-
-.PARAMETER elevated
-	Check if we're running as admin.
+    This script will do the following:
+    1. Opens Admin Powershell Instance
+    2. Finds entries in hosts
+    3. Creates array of entries
+    4. Comments all found entries
+    5. Searches for commented entry
+    6. If Commented but desired entry is found
+    7. Uncomments desired entry
+    8. Else, Create desired entry.
 
 .EXAMPLE
-    ".\hostsControl.ps1 #{serverip} #{wwwdomain}"
+    ".\hostsControl.ps1" *NOTE* Designed to only be called by Octopus Deploy, ran on worker node.
 
 .NOTES
     Script Name: hostsControl
@@ -24,17 +22,21 @@
 #>
 <#
 Define Variables
-    octoHost    = ServerIP + Domain
-    elevated    = To determine if we're an admin.
+    serverip    = global variable from Octopus
+    wwwdomain   = global variable from Octopus
+    octoHost    = ServerIP + (space) + Domain
     hostsPath   = path to hosts file.
-    hostsEntry  = used to make hosts entries.
     hostsActive = Array of existing active hosts entries.
     pattern     = RegEx pattern of un-commented hosts entries (IP + domain/comment)
 #>
-param(
-    [string]$octoHost
-    [switch]$elevated
-)
+
+# Check if globals have values.  If so, create octoHost.
+if (($serverip -eq $null) -or ($wwwdomain -eq $null)) {
+    Write-Error "hostsControl.ps1 - ERROR Serverip or wwwdomain are equal to NULL"
+    throw 'hostsControl.ps1 - serverip or wwwdomain are empty.'
+} else {
+    $octoHost = "$($serverip) $($wwwdomain)"
+}
 
 $hostsPath = "$env:SystemDrive\Windows\System32\Drivers\etc\hosts"
 $hostsActive = @()
@@ -42,69 +44,41 @@ $pattern = '^(?<IP>\d{1,3}(\.\d{1,3}){3})\s+(?<Host>.+)$'
 
 <#
 Define Functions
-    Test-Admin          = Check if we're admin.
     Open-PSConnection   = Opens a Powershell instance as admin.
-    Get-HostsEntries    = Reads hosts file and returns un-commented hosts file entries.
-    Test-HostsEntries   = Error corrects if there's too many hosts entries, and comments them.
-    Set-UpdateHosts     = Sets the hosts entry.
+    Set-HostsEntries    = Does everything else.
 #>
-function Test-Admin {
-    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
 
 function Open-PSConnection {
-    if ((Test-Admin) -eq $false)  {
-    if ($elevated) {
-        Write-Error "hostsControl.ps1 - Tried to create elevated Powershell instance, did not work, aborting..."
-    }else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated -action "{1}"' -f ($myinvocation.MyCommand.Definition,$action))
-        }
-    exit
-    }
-    Write-Host "hostsControl.ps1 - Elevated instance of Powershell created."
+    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 }
 
-function Get-HostsEntries {   
+function Set-HostsEntries {   
     (Get-Content -Path $hostsPath)  | ForEach-Object {
         If ($_ -match $pattern) {
             $hostsActive += "$($matches.IP) $($matches.Host)"
         }
     }
-    
-    Write-Host "hostsControl.ps1 - Found the following host entries:"
-    for ($i = 0; $i -le ($hostsActive.length - 1); $i += 1) {
-        Write-Host "hostsControl.ps1 - $($hostsActive[$i])"
-    }
-}
-
-function Test-HostsEntries {
+    Write-Host "hostsControl.ps1 - number of entries in hostsActive: $($hostsActive.length)" #REMOVE
     if ($hostsActive.length -ge 1) {
-        Write-Host "hostsControl.ps1 - Active hosts entries found, commenting them..."
-        for ($i = 0; $i -le ($hostsActive.length - 1); $i += 1) {
-            (Get-Content $hostsPath -Raw) -replace "$($hostsActive[$i])", "#$($hostsActive[$i])" | Set-Content -Path $hostsPath
-            Write-Information "hostsControl.ps1 - Commented $($hostsActive[$i])"
+        Write-Host "hostsControl.ps1 - Found the following host entries:"
+        for ($i = 0; $i -le ($hostsActive.length - 1); $i++) {
+            Write-Host "hostsControl.ps1 - Commenting: $($hostsActive[$i])"
+            (Get-Content $hostsPath -Raw) -replace "$($hostsActive[$i])", "# $($hostsActive[$i])" | Set-Content -Path $hostsPath
         }
-        Write-Host "hostsControl.ps1 - Finished.  All previous hosts entries have been commented."
-    }else {
-        Write-Host "hostsControl.ps1 - No active hosts entries found, proceeding..."
+        Write-Host "hostsControl.ps1 - All previous hosts entries have been commented."
     }
-}
-
-function Set-UpdateHosts {
-    if (Select-String -Path $hostsPath -Pattern "#$($octoHost)" -Quiet){
-        (Get-Content $hostsPath -Raw) -replace "#$($octoHost)", "$($octoHost)" | Set-Content -Path $hostsPath
+    if (Select-String -Path $hostsPath -Pattern "# $($octoHost)" -Quiet) {
+        Write-Host "hostsControl.ps1 - Uncommenting: $($octoHost)"
+        (Get-Content $hostsPath -Raw) -replace "# $($octoHost)", "$($octoHost)" | Set-Content -Path $hostsPath
     }else {
+        Write-Host "hostsControl.ps1 - Entry not found, creating..."
+        Write-Host "hostsControl.ps1 - Adding to hosts: $($octoHost)"
         Add-Content -path $hostsPath -value $octoHost;
     }
 }
 
 # Run Program
-Test-Admin
 Open-PSConnection
-Get-HostsEntries
-Test-HostsEntries
-Set-UpdateHosts 
+Set-HostsEntries
 
-# Done! Close PS Instance.
-Stop-Process -Name "powershell"
+# Done! Octopus will automatically close the PS Instance.
